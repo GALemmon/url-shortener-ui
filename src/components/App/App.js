0905f2eaf802f6290df3bc7517748d45594f7bc5
George Lemmon <george.lemmon@gmail.com>
2022-04-26 11:42:12 -0600

    Add count to trigger rerender after post

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
-import { getUrls } from '../../apiCalls'
+import { getUrls, postUrl } from '../../apiCalls'
 import UrlContainer from '../UrlContainer/UrlContainer'
 import UrlForm from '../UrlForm/UrlForm'
 
 const App = () => {
   const [urls, setUrls] = useState([])
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
     fetchUrls()
-  }, [])
+  }, [count])
 
   const fetchUrls = () => {
-    getUrls()
-    .then((urlData) => {
+    getUrls().then((urlData) => {
       const flat = Object.values(urlData)
       setUrls(flat)
     })
   }
+
+  const postNewUrl = (longUrl, title) => {
+    postUrl(longUrl, title).then(setCount((previous) => previous + 1))
+  }
+
   console.log('App urls: ', urls)
-  
+
   return (
     <main className='App'>
       <header>
         <h1>URL Shortener</h1>
-        <UrlForm />
+        <UrlForm postNewUrl={postNewUrl} />
       </header>
-
       <UrlContainer urls={urls} />
     </main>
   )
