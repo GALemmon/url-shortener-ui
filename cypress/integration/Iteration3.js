2e3ce399d6aa0522d6d78f35d2574dc8862e7981
George Lemmon <george.lemmon@gmail.com>
2022-04-26 15:37:10 -0600

    Add fancified tests to iteration 3 and 4

--- cypress/integration/Iteration3.js
+++ cypress/integration/Iteration3.js
@@ -1,20 +1,33 @@
 describe('Main page Tests', () => {
-  beforeEach(() => {})
-
   it('should render the main page', () => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
-      response: 200,
-      fixture: 'stub.json'
+      fixture: './stub'
     })
 
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000/')
       .get('.title')
       .should('contain', 'URL Shortener')
-      .get('.container')
+  })
+
+  it('should have an existing url card', () => {
+    cy.get('.container')
       .children()
       .should('have.length', 1)
       .get('.url')
       .should('contain', 'Awesome photo')
   })
-})
 
+  it('should have text inputs', () => {
+    cy.get('input[name="title"]')
+      .get('input[name="urlToShorten"]')
+      .get('button[name="submit-button"')
+  })
+
+  it('should allow text input', () => {
+    cy.get('input[name="title"]').type('Title').should('have.value', 'Title')
+
+    cy.get('input[name="urlToShorten"]')
+      .type('https://docs.cypress.io/api/commands/first#Usage')
+      .should('have.value', 'https://docs.cypress.io/api/commands/first#Usage')
+  })
+})
--- cypress/integration/iteration4.js
+++ cypress/integration/iteration4.js
@@ -1,13 +1,25 @@
-describe('Main page Tests', () => {
-  beforeEach(() => {})
-
-  it('should render the main page', () => {
+describe('POST request tests', () => {
+  it('should have an existing url card', () => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
-      response: 200,
-      fixture: 'stub.json'
+      fixture: './stub',
+      times: 3
     })
-
     cy.visit('http://localhost:3000')
+      .get('.container')
+      .children()
+      .should('have.length', 1)
+      .get('.url')
+      .should('contain', 'Awesome photo')
+  })
+
+  it('should add a new url card', () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
+      fixture: './urls',
+      times: 3
+    })
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
+      fixture: './url'
+    })
       .get('input[name="title"]')
       .type('Title')
       .get('input[name="urlToShorten"]')
@@ -16,8 +28,11 @@ describe('Main page Tests', () => {
       .click()
       .get('.container')
       .children()
-      .should('have.length', 1)
+      .should('have.length', 2)
+      .get('.container div:first')
+      .should('contain', 'Awesome photo')
+      .get('.container div:last')
+      .should('contain', 'Title')
+      .and('contain', 'http://localhost:3001/useshorturl/2')
   })
 })
-
-
