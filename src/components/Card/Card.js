080cc650eca3bfbb167137f64868b11a551df8f6
George Lemmon <george.lemmon@gmail.com>
2022-04-26 11:42:30 -0600

    Fix dot notation

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,12 +3,12 @@ import './Card.css'
 
 const Card = ({ url }) => {
   return (
-    <div className='url' >
-      <h3>{url[0].title}</h3>
-      <a href={url[0].short_url} target='blank'>
-        {url[0].short_url}
+    <div className='url'>
+      <h3>{url.title}</h3>
+      <a href={url.short_url} target='blank'>
+        {url.short_url}
       </a>
-      <p>{url[0].long_url}</p>
+      <p>{url.long_url}</p>
     </div>
   )
 }
